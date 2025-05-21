b50df3c94c82ab1e97ac1d3298efb7c113331cc5
eto <eto.saviour80@gmail.com>
2025-05-21 23:38:16 +0100

    update

--- resources/views/dashboard/header.blade.php
+++ resources/views/dashboard/header.blade.php
@@ -1087,8 +1087,8 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
         </a>
         <div class="d-flex">
-                        <span class = "m-auto ms-0">Server Date: </span><b class="m-auto ms-0" id="formattedDateOutput">
-                        </b>
+                        {{-- <span class = "m-auto ms-0">Server Date: </span><b class="m-auto ms-0" id="formattedDateOutput"> --}}
+                      <img id="profile-img" style="width:180px;" class="profile-img-card" src="assets/images/logo.png" />  </b>
         </div>
         
         <ul class="navbar-item flex-row ms-lg-auto ms-0">
